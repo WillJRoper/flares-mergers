69c9a5d6fd5460fa71d47927b33b07a94e9c5ce7
wjr21 <wjr21@sussex.ac.uk>
2024-08-15 15:16:21 +0100

    Added progenitor panel

--- Code/plot_pair_dists.py
+++ Code/plot_pair_dists.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from unyt import kpc, Mpc
 from scipy.spatial import cKDTree
 
-from utils import savefig, to_physical
+from utils import savefig, to_physical, unpack_progenitors, PROG_SNAPS
 
 
 # Define the inputs
@@ -42,6 +42,9 @@ args = parser.parse_args()
 
 # Include units on the distance and convert to Mpc
 dist = (args.dist * kpc).to(Mpc).value
+
+# Get progenitor information
+start_inds, nprogs = unpack_progenitors(args.master_file)
 
 # Loop over regions and snapshots calculating the pair distances
 pair_dists = {}
@@ -82,11 +85,89 @@ with h5py.File(args.master_file, "r") as hdf:
             # Store the distances
             pair_dists[snap].extend(dists)
 
-# Plot the histogram for each snapshot
+# Loop over regions and snapshots calculating the distances between progenitors
+prog_pair_dists = {}
+with h5py.File(args.master_file, "r") as hdf:
+    for reg in tqdm(hdf.keys(), desc="Regions"):
+        for snap in hdf[reg].keys():
+            # Can't do the first snapshot since there are no progenitors
+            if snap not in PROG_SNAPS:
+                continue
+
+            # Get the prog snap
+            prog_snap = PROG_SNAPS[snap]
+
+            # Get this snapshots progenitor data
+            this_start_inds = start_inds[snap]
+            this_nprogs = nprogs[snap]
+
+            # Create an entry for the snapshot
+            prog_pair_dists.setdefault(prog_snap, [])
+
+            # Extract the redshift
+            z = float(prog_snap.split("z")[-1].replace("p", "."))
+
+            # Get the galaxy group
+            gal_grp = hdf[f"{reg}/{prog_snap}/Galaxy"]
+
+            # Get the positions and convert to physical units
+            pos = to_physical(gal_grp["COP"][:].T, z)
+
+            # Get the masses
+            mass = gal_grp["Mstar_aperture/30"][:] * 10**10
+
+            # Get the distances for the progenitors
+            dists = []
+            for i in range(len(this_start_inds)):
+                inds = list(
+                    range(
+                        this_start_inds[i], this_start_inds[i] + this_nprogs[i]
+                    )
+                )
+
+                # Get the mass and positions for these progenitors
+                prog_mass = mass[inds]
+                prog_pos = pos[inds, :]
+
+                # Mask out progentiros below 10^8 Msun
+                mask = prog_mass > 10**8
+                prog_pos = prog_pos[mask]
+
+                # Create a KDTree
+                tree = cKDTree(prog_pos)
+
+                # Query the tree
+                pairs = tree.query_pairs(dist, output_type="set")
+
+                # Calculate the distances
+                prog_dists = (
+                    np.array(
+                        [
+                            np.linalg.norm(prog_pos[i] - prog_pos[j])
+                            for i, j in pairs
+                        ]
+                    )
+                    * 1000
+                )
+
+            # Store the distances
+            prog_pair_dists[prog_snap].extend(prog_dists)
+
+
+# Plot the histogram for each snapshot in two panels (one above the other)
 fig, ax = plt.subplots(figsize=(3.5, 3.5))
+gs = fig.add_gridspec(
+    2, 2, hspace=0.0, height_ratios=[1, 1], width_ratios=[10, 1]
+)
+ax = fig.add_subplot(gs[0, 0])
+ax1 = fig.add_subplot(gs[1, 0])
+cax = fig.add_subplot(gs[:, 1])
 ax.loglog()
+ax1.loglog()
 ax.grid(True)
+ax1.grid(True)
 ax.set_axisbelow(True)
+ax1.set_axisbelow(True)
 
 # Create a colormap for each redshift
 zs = np.arange(5, 16, 1)
@@ -107,13 +188,42 @@ for i, snap in enumerate(sorted(pair_dists.keys())):
         color=colors[i],
     )
 
-ax.set_xlabel("$R_{i,j} / $ [pkpc]")
+    # Plot the histogram for the progenitors
+    ax1.hist(
+        prog_pair_dists[snap],
+        bins=bins,
+        histtype="step",
+        color=colors[i],
+    )
+
+# Put a text box in the top left corner
+ax.text(
+    0.05,
+    0.95,
+    "All Galaxies",
+    transform=ax.transAxes,
+    fontsize=8,
+    verticalalignment="top",
+    bbox=dict(facecolor="white", alpha=0.5, boxstyle="round,pad=0.5"),
+)
+ax1.text(
+    0.05,
+    0.95,
+    "Mergers",
+    transform=ax1.transAxes,
+    fontsize=8,
+    verticalalignment="top",
+    bbox=dict(facecolor="white", alpha=0.5, boxstyle="round,pad=0.5"),
+)
+
+ax1.set_xlabel("$R_{i,j} / $ [pkpc]")
 ax.set_ylabel("$N$")
+ax1.set_ylabel("$N$")
 
 # Create the colorbar for the redshifts
 cbar = fig.colorbar(
     plt.cm.ScalarMappable(cmap="viridis"),
-    ax=ax,
+    cax=cax,
     ticks=np.linspace(0, 1, len(zs)),
 )
 cbar.ax.set_yticklabels(reversed([f"{z:.0f}" for z in zs]))
--- Code/utils.py
+++ Code/utils.py
@@ -1,6 +1,27 @@
 """General utlity functions for the project."""
 import os
 import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+
+# Define the snapshots list
+SNAPSHOTS = [
+    "000_z015p000",
+    "001_z014p000",
+    "002_z013p000",
+    "003_z012p000",
+    "004_z011p000",
+    "005_z010p000",
+    "006_z009p000",
+    "007_z008p000",
+    "008_z007p000",
+    "009_z006p000",
+    "010_z005p000",
+]
+
+PROG_SNAPS = {
+    snap: prog_snap for snap, prog_snap in zip(SNAPSHOTS[1:], SNAPSHOTS[:-1])
+}
 
 
 def savefig(fig, outname):
@@ -39,3 +60,45 @@ def to_physical(pos, redshift):
         array: The physical position.
     """
     return pos / (1 + redshift)
+
+
+def unpack_progenitors(filepath):
+    """
+    Read the progenitor information from the file and make it more accessible.
+
+    Args:
+        filepath (str): The path to the file.
+
+    Returns:
+        dict: The progenitor information.
+    """
+    # Create a dictionaries to store the progenitors
+    start_indexes = {}
+    nprogenitors = {}
+
+    # Open the file
+    with h5py.File(filepath, "r") as hdf:
+        for reg in hdf.keys():
+            for snap in hdf[reg].keys():
+                # Make entries for this snapshot
+                start_indexes.setdefault(snap, [])
+                nprogenitors.setdefault(snap, [])
+
+                # Get galaxy group
+                gal_group = hdf[reg][snap]["Galaxy"]
+
+                # Get mergergraph group
+                merger_group = gal_group[reg][snap]["MergerGraph"]
+
+                # Get the data
+                starts = merger_group["Prog_Start_Index"]
+                nprogs = merger_group["NProgs"]
+
+                # Shift the start indexes
+                starts = starts + np.sum(nprogenitors[snap])
+
+                # Store the data
+                start_indexes[snap].extend(starts)
+                nprogenitors[snap].extend(nprogs)
+
+    return start_indexes, nprogenitors
